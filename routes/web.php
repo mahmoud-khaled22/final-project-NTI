fcd84b4aac13fedbb52cbd200fa00fb519d0074c
mohammedkhedrr <mkh1772004@gmail.com>
2025-09-19 01:05:44 +0200

    الاسم

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@ use App\Http\Controllers\CoursesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\register;
 use App\Http\Controllers\UserController;
-<<<<<<< HEAD
 use App\Http\Controllers\login;
-=======
 use App\Models\Course;
->>>>>>> 8c0ba10ded77ba49304a087ad8896dafc6fd14e3
 
 Route::get('/', function () {
     return view('welcome');
